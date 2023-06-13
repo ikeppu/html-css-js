11bd24e282861801101e0733b0f3c4d71a479dc8
ikeppu <andrejs.sahniks@gmail.com>
2023-06-13 22:25:38 +0300

    draggabe menu

---
+++ draggable-tab-menu/main.js
@@ -0,0 +1,68 @@
+const btnLeft = document.querySelector(".left-btn");
+const btnRight = document.querySelector(".right-btn");
+const tabMenu = document.querySelector(".tab-menu");
+
+const IconVisibility = () => {
+  let scrollLeftValue = Math.ceil(tabMenu.scrollLeft);
+
+  let scrollableWidth = tabMenu.scrollWidth - tabMenu.clientWidth;
+
+  btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
+  btnRight.style.display = scrollableWidth > scrollLeftValue ? "block" : "none";
+};
+
+btnRight.addEventListener("click", () => {
+  tabMenu.scrollLeft += 150;
+
+  setTimeout(() => IconVisibility(), 100);
+});
+btnLeft.addEventListener("click", () => {
+  tabMenu.scrollLeft -= 150;
+  setTimeout(() => IconVisibility(), 100);
+});
+
+window.onload = function () {
+  btnRight.style.display =
+    tabMenu.scrollWidth > tabMenu.clientWidth ||
+    tabMenu.scrollWidth >= window.innerWidth
+      ? "block"
+      : "none";
+
+  btnLeft.style.display =
+    tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
+};
+
+window.onresize = function () {
+  btnRight.style.display =
+    tabMenu.scrollWidth > tabMenu.clientWidth ||
+    tabMenu.scrollWidth >= window.innerWidth
+      ? "block"
+      : "none";
+
+  btnLeft.style.display =
+    tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
+
+  let scrollLeftValue = Math.round(tabMenu.scrollLeft);
+
+  btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
+};
+
+// Make tab menu draggable by javascriprt
+let activeDrag = false;
+
+tabMenu.addEventListener("mousemove", (drag) => {
+  if (!activeDrag) return;
+
+  tabMenu.scrollLeft -= drag.movementX;
+  IconVisibility();
+  tabMenu.classList.add("dragging");
+});
+
+document.addEventListener("mouseup", () => {
+  activeDrag = false;
+  tabMenu.classList.remove("dragging");
+});
+
+tabMenu.addEventListener("mousedown", () => {
+  activeDrag = true;
+});
